bc3f8f2db76432d7a5d1039fc5c110a4b642527a
TakuyaHarayama <harayama@ijgn.jp>
2017-11-06 01:13:01 +0900

    Update team views

--- app/models/team.rb
+++ app/models/team.rb
@@ -23,4 +23,20 @@ class Team < ApplicationRecord
   def posts
     Post.includes(:user).where(user_id: users.ids)
   end
+
+  def post_activities_rate
+    max = users.size * number_of_days_created
+    (posts.size.to_f / max * 100).round(1)
+  end
+
+  def most_starred_post
+    max = posts.maximum(:stars_count)
+    posts.find_by('stars_count >= ?', max)
+  end
+
+  private
+
+  def number_of_days_created
+    (Date.current - created_at.to_date).to_i
+  end
 end
